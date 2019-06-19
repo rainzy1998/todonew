fce2e05c01fd4aedeac973670532776eea67a1f3
zy <Rain_zhuang@163.com>
2019-06-19 09:22:00 +0800

    Initial commit

---
+++ todolist/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name='todolist'
+
+urlpatterns = [
+    path('home/',views.home,name='主页'),
+    path('edit/<int:forloop_counter>',views.edit,name='编辑'),
+    path('about/',views.about,name='关于'),
+    path('del/<forloop_counter>',views.delete,name='删除')
+    ]
---
+++ todolist/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+lst = [
+    {'待办事项': '吃饭', '已完成': False},
+    {'待办事项': '睡觉', '已完成': False}
+]
+
+
+def home(request):
+    global lst
+    print(request.method)
+
+    if request.method == "POST":
+        content = request.POST.get('待办事项')
+        print(content)
+        if not content and content.strip() == '':
+            return render(request, 'todolist/home.html',
+                          {'清单': lst, '警告': '请输入内容'})
+        else:
+            lst.append({'待办事项': content, '已完成': False})
+            return render(request, 'todolist/home.html', {'清单': lst})
+    else:
+        return render(request, 'todolist/home.html', {'清单': lst})
+
+
+def edit(request, forloop_counter):
+    if request.method == "POST":
+        content = request.POST['已修改事项']
+        if not content or content.strip() == '':
+            return render(request, 'todolist/edit.html',
+                          {'警告': '请输入内容'})
+        else:
+            lst[forloop_counter - 1]['待办事项'] = content
+            return redirect('todolist:主页')
+    else:
+        content = lst[forloop_counter - 1]['待办事项']
+        return render(request, 'todolist/edit.html', {'待修改事项': content})
+
+
+def about(request):
+    return render(request, 'todolist/about.html')
+
+
+def delete(request, forloop_counter):
+    if request.method == "POST":
+        forloop_counter = int(forloop_counter) - 1
+        print(forloop_counter)
+        lst.pop(forloop_counter)
+    return redirect('todolist:主页')
